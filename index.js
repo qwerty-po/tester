d3d7eae9db1b6a4e9a5642482486052d0cb2a79b
qwerty-po <71523377+qwerty-po@users.noreply.github.com>
2022-01-13 15:12:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
   res.header("Access-control-allow-origin", "http://localhost:3000");
   res.send(data);
 })
